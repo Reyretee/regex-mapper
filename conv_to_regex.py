5e0e5146c01d4a9626c818f22f12a8c470cdb29a
Ahmetcan Dinçer <ahmet07c09@gmail.com>
2024-09-28 17:29:38 +0300

    Refactored main.py to move conv_to_regex function to separate file, improved error handling, and updated output.yml file

---
+++ conv_to_regex.py
@@ -0,0 +1,14 @@
+from char_map import char_map
+import re
+
+def conv_to_regex(word):
+    word = re.escape(word)
+    regex_word = ''
+    
+    for char in word:
+        if char in char_map:
+            regex_word += char_map[char]
+        else:
+            regex_word += f'[{char}]'
+    
+    return fr'\b{regex_word}\b'
--- main.py
+++ main.py
@@ -1,65 +1,35 @@
-import re
-import yaml
 import os
-
-
-char_map = {
-    'a': '[aáâä@4]',
-    'b': '[b8]',
-    'c': '[cç]',
-    'd': '[d]',
-    'e': '[eéê3]',
-    'f': '[f]',
-    'g': '[gğ9]',
-    'h': '[h]',
-    'i': '[i1!İ]',
-    'j': '[j]',
-    'k': '[kq]',
-    'l': '[l1]',
-    'm': '[m]',
-    'n': '[n]',
-    'o': '[o0öó]',
-    'p': '[p]',
-    'r': '[r]',
-    's': '[sş5$]',
-    't': '[t]',
-    'u': '[uüûú]',
-    'v': '[v]',
-    'y': '[y]',
-    'z': '[z2]'
-}
-
-def conv_to_regex(word):
-    word = re.escape(word)
-    regex_word = ''
-    
-    for char in word:
-        regex_word  += char_map.get(char.lower(), f'[{char}]')
-    return fr'\b{regex_word}\b'
+import yaml
+from conv_to_regex import conv_to_regex
 
 input_file = 'words.txt'
 output_file = 'output.yml'
 
-if not os.path.isfile(input_file):
+try:
+    if not os.path.isfile(input_file):
+        print(f'File {input_file} not found')
+    else:
+        with open(input_file, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+
+        regex_list = tuple(conv_to_regex(word.strip()) for word in lines if word.strip())
+        
+        with open(output_file, 'w', encoding='utf-8') as yaml_file:
+            yaml.dump(
+                {
+                    'regex_list': regex_list
+                },
+                yaml_file,
+                allow_unicode=True,
+                default_flow_style=False,
+                sort_keys=False
+            )
+        print("Output file created successfully.")
+except FileNotFoundError:
     print(f'File {input_file} not found')
-else:
-    with open(input_file, 'r', encoding='utf-8') as f:
-        lines = f.readlines()
-
-    regex_list = []
-    for word in lines:
-        word = word.strip()
-        if word:
-            regex_list.append(conv_to_regex(word))
-
-    with open(output_file, 'w', encoding='utf-8') as yaml_file:
-        yaml.dump(
-            {
-                'regex_list': regex_list
-            },
-            yaml_file,
-            allow_unicode=True,
-            default_flow_style=False,
-            sort_keys=False
-        )
-    print("output.yml dosyası oluşturuldu.")
+except IOError as e:
+    print(f'Error reading input file: {e}')
+except yaml.YAMLError as e:
+    print(f'Error creating YAML file: {e}')
+except Exception as e:
+    print(f'An unknown error occurred: {e}')
